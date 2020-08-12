7931002b2e4ed577eda972f57f2ef1178b24d3fa
videoiurex <videoiurex@gmail.com>
2020-08-12 12:18:11 +0200

    sustituido el echo por nano

--- bios/nuevousuario.sh
+++ bios/nuevousuario.sh
@@ -3,4 +3,4 @@ set -x
 #Crear usauario personal
 useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner,wheel -s /bin/bash zeus
 passwd zeus
-echo %wheel ALL=(ALL) ALL >> /etc/sudoers
+nano /etc/sudoers
