ae404cbe3af1e11fcda2a16cd6213390c6cd5345
videoiurex <videoiurex@gmail.com>
2020-08-12 10:54:24 +0200

    Usuario añadido a sudo

--- bios/nuevousuario.sh
+++ bios/nuevousuario.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
 #Crear usauario personal
-useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner -s /bin/bash zeus
+useradd -m -g users -G audio,lp,optical,storage,video,wheel,games,power,scanner,wheel -s /bin/bash zeus
 passwd zeus
+echo %wheel ALL=(ALL) ALL >> /etc/sudoers
