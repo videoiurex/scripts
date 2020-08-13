ce988c6a2b0276e888cae5dea4fde203a79b285e
videoiurex <videoiurex@gmail.com>
2020-08-13 10:12:19 +0000

    formataera añadido a bios

--- bios/particionar.sh
+++ bios/particionar.sh
@@ -4,5 +4,5 @@ parted /dev/sda mklabel msdos
 parted /dev/sda mkpart primary ext2 1MiB 512MiB
 parted /dev/sda set 1 boot on
 parted /dev/sda mkpart primary linux-swap 512MiB 4.5GiB
-parted /dev/sda mkpart primary ext4 4.5GiB 25.5MiB
-parted /dev/sda mkpart primary ext4 24.5MiB 100%
+parted /dev/sda mkpart primary ext4 4.5GiB 25.5GiB
+parted /dev/sda mkpart primary ext4 25.5GiB 100%
