6011f02cb6a0cde21b3d6555ee08bdb256e7b9b4
videoiurex <videoiurex@gmail.com>
2020-08-13 11:51:16 +0200

    añadido el particionar a bios

---
+++ bios/particionar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+parted /dev/sda mklabel msdos 
+parted /dev/sda mkpart primary ext2 1MiB 512MiB
+parted /dev/sda set 1 boot on
+parted /dev/sda mkpart primary linux-swap 512MiB 4.5GiB
+parted /dev/sda mkpart primary ext4 4.5GiB 25.5MiB
+parted /dev/sda mkpart primary ext4 24.5MiB 100%
