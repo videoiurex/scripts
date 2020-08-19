d392ce30a7a27127a91925d41470c724458c7e14
videoiures <videoiures@gmail.com>
2020-08-19 17:27:36 +0200

    añadiendo interacción

--- bios/installarchbios.sh
+++ bios/installarchbios.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-
+clear
+echo Particionar disco (debe ser de al menos 60GB):
+echo parted /dev/sda mklabel msdos
+echo parted /dev/sda mkpart primary ext2 1MiB 512MiB
+echo parted /dev/sda set 1 boot on
+echo parted /dev/sda mkpart primary linux-swap 512MiB 4.5GiB
+echo parted /dev/sda mkpart primary ext4 4.5GiB 25.5GiB
+echo iparted /dev/sda mkpart primary ext4 25.5GiB 100%
+read -p "Pulsa ENTER para continuar..."
 #particionar disco (debe ser de al menos 60GB)
 parted /dev/sda mklabel msdos
 parted /dev/sda mkpart primary ext2 1MiB 512MiB
@@ -9,6 +17,14 @@ parted /dev/sda mkpart primary linux-swap 512MiB 4.5GiB
 parted /dev/sda mkpart primary ext4 4.5GiB 25.5GiB
 parted /dev/sda mkpart primary ext4 25.5GiB 100%
 
+clear
+echo formatear particiones
+echo mkfs.ext2 /dev/sda1
+echo mkswap /dev/sda2
+echo swapon /dev/sda2
+echo mkfs.ext4 /dev/sda3
+echo mkfs.ext4 /dev/sda4
+read -p "Pulsa ENTER para continuar..."
 #formatear particiones
 mkfs.ext2 /dev/sda1
 mkswap /dev/sda2
@@ -16,6 +32,12 @@ swapon /dev/sda2
 mkfs.ext4 /dev/sda3
 mkfs.ext4 /dev/sda4
 
+echo montar particiones
+echo mount /dev/sda3 /mnt
+echo mkdir /mnt/home
+echo mount /dev/sda4 /mnt/home
+echo mkdir /mnt/boot
+echo mount /dev/sda1 /mnt/boot
 #montar particiones
 mount /dev/sda3 /mnt
 mkdir /mnt/home
@@ -23,8 +45,12 @@ mount /dev/sda4 /mnt/home
 mkdir /mnt/boot
 mount /dev/sda1 /mnt/boot
 
+echo Instalar paquetes básicos
+echo pacstrap /mnt linux-lts linux-firmware linux-lts-headers base base-devel nano os-prober \
+echo grub ntfs-3g networkmanager
 #Instalar paquetes básicos
-pacstrap /mnt linux-lts linux-firmware linux-lts-headers base base-devel nano os-prober grub ntfs-3g networkmanager
+pacstrap /mnt linux-lts linux-firmware linux-lts-headers base base-devel nano os-prober \
+grub ntfs-3g networkmanager
 
 #Crear fstab
 genfstab /mnt >> /mnt/etc/fstab
